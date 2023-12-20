a269921673261847f7739d45e562bb55470edbb9
babra <babra@gsoftconsulting.com>
2023-12-20 18:03:22 +0500

    initial commit

---
+++ src/common/config/mongodb.config.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.MONGO_URL,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class MongoModule {}
